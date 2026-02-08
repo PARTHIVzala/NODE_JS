6864174d23aa37becf28882f83944dcfc784e147
PARTHIVzala <166072111+PARTHIVzala@users.noreply.github.com>
2026-02-08 22:59:38 +0530

    Update
    Refactor database connection to use event listeners for connection status.

--- PR-4 BOOK-STORE/config/dbconnection.js
+++ PR-4 BOOK-STORE/config/dbconnection.js
@@ -1,7 +1,15 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+
 const dbconnect = () => {
-    mongoose.connect("mongodb://localhost:27017/Node-PR")
-        .then(() => console.log('DB is Connected !!'))
-        .catch((err) => console.log(err))
-}
+  mongoose.connect("mongodb://localhost:27017/Node-PR");
+
+  mongoose.connection.on("connected", () => {
+    console.log("DB is Connected !!");
+  });
+
+  mongoose.connection.on("error", (err) => {
+    console.log(err);
+  });
+};
+
 module.exports = dbconnect;
